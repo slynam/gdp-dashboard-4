cafed2cf3dde822a1e7ab3142ca43a524b0455c2
slynam <94933784+slynam@users.noreply.github.com>
2024-11-19 08:45:17 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import streamlit as st
 from pathlib import Path
 
 import data_sources
-from data_sources import big_query, snowflake, aws_s3_boto, google_sheet
+from data_sources import google_sheet
 
 from utils import ui, intro
 
@@ -14,30 +14,6 @@ DATA_SOURCES = {
         "secret_key": None,
         "docs_url": None,
         "get_connector": None,
-    },
-    "🔎  BigQuery": {
-        "module": data_sources.big_query,
-        "secret_key": "bigquery",
-        "docs_url": "https://docs.streamlit.io/knowledge-base/tutorials/databases/bigquery",
-        "get_connector": data_sources.big_query.get_connector,
-        "tutorial": data_sources.big_query.tutorial,
-        "tutorial_anchor": "#tutorial-connecting-to-bigquery",
-    },
-    "❄️ Snowflake": {
-        "module": data_sources.snowflake,
-        "secret_key": "snowflake",
-        "docs_url": "https://docs.streamlit.io/knowledge-base/tutorials/databases/snowflake",
-        "get_connector": data_sources.snowflake.get_connector,
-        "tutorial": data_sources.snowflake.tutorial,
-        "tutorial_anchor": "#tutorial-connecting-to-snowflake",
-    },
-    "📦 AWS S3": {
-        "module": data_sources.aws_s3_boto,
-        "secret_key": "aws_s3",
-        "docs_url": "https://docs.streamlit.io/knowledge-base/tutorials/databases/aws-s3",
-        "get_connector": data_sources.aws_s3_boto.get_connector,
-        "tutorial": data_sources.aws_s3_boto.tutorial,
-        "tutorial_anchor": "#tutorial-connecting-to-aws-s3",
     },
     "📝 Google Sheet": {
         "module": data_sources.google_sheet,
